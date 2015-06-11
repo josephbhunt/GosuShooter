bfa9a4241611d46390000750e5957a4fa747a0bb
Joseph Hunt <josephb.hunt@gmail.com>
2015-06-11 01:15:12 -0400

    Background scrolls down.

--- gosu_shooter.rb
+++ gosu_shooter.rb
@@ -7,6 +7,7 @@ require "./collidable"
 require "./explosion"
 require "./enemies_generator"
 require "./item"
+require 'ap'
 
 class MyWindow < Gosu::Window
   include GameConstants
@@ -16,7 +17,8 @@ class MyWindow < Gosu::Window
     self.caption = 'Space Shooter'
 
     @font = Gosu::Font.new(20)
-    @background_image = Gosu::Image.new("bg_blue.jpg")
+    @background = Background.new
+
     player_start_x = WINDOW_WIDTH / 2
     player_start_y = WINDOW_HEIGHT - 80
     @player = Player.new(player_start_x, player_start_y)
@@ -26,6 +28,7 @@ class MyWindow < Gosu::Window
   end
 
   def update
+    @background.update
     close  if Gosu::button_down?(Gosu::KbEscape)
     @player.update  if @player.live?
     
@@ -60,7 +63,7 @@ class MyWindow < Gosu::Window
   end
 
   def draw
-    @background_image.draw(0, 0, 0)
+    @background.draw
     Shot.draw_all
     @player.draw
     @enemy_gen.draw
@@ -73,6 +76,53 @@ class MyWindow < Gosu::Window
 
   def game_over
     @font.draw("GAME OVER!", (WINDOW_WIDTH / 2.5), (WINDOW_HEIGHT / 2), 2, 2.0, 2.0, 0xff_ffff00)
+  end
+end
+
+class Background
+  include GameConstants
+  SCROLL_RATE = 0.5
+
+  TILE_SIZE = 90
+
+  def initialize
+    @background_images = Gosu::Image.load_tiles("bg_blue.jpg", TILE_SIZE, TILE_SIZE, tileable: true)
+    @bg_grid = Array.new(WINDOW_WIDTH / TILE_SIZE) {Array.new(WINDOW_HEIGHT / TILE_SIZE + 1)}
+    @bg_grid.each_with_index do |row, x|
+      row.each_with_index do |col, y|
+        image_x = x * TILE_SIZE
+        image_y = y * TILE_SIZE
+        y_multiplyer = WINDOW_WIDTH / TILE_SIZE
+        gt_image = @background_images[y * y_multiplyer + x]
+        grid_tile = GridTile.new(x, y - TILE_SIZE, image_x, image_y, gt_image)
+        @bg_grid[x][y] = grid_tile
+      end
+    end
+  end
+
+  def update
+    @bg_grid.each{|bg_row| bg_row.each{|t| t.move_down(SCROLL_RATE)}}
+  end
+
+  def draw
+    @bg_grid.each {|t_row| t_row.each {|t| t.draw  unless t.nil? }}
+  end
+end
+
+class GridTile
+
+  attr_accessor :grid_x, :grid_y, :image_x, :image_y, :image
+
+  def initialize(grid_x, grid_y, image_x, image_y, image=nil)
+    @grid_x, @grid_y, @image_x, @image_y, @image = grid_x, grid_y, image_x, image_y, image
+  end
+
+  def move_down(rate=1)
+    @image_y += rate
+  end
+
+  def draw
+    image.draw(@image_x, @image_y, 1)  unless image.nil?
   end
 end
 
